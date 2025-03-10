002ee532e274440b455f7de87339bb4fa3c0598a
seeyeon <gulf72pr@swu.ac.kr>
2025-03-10 16:07:04 +0900

    refactor: Kotlin과 Java의 getter 변환 차이 수정

--- backend/src/main/java/com/pofo/backend/domain/mapper/ProjectMapper.java
+++ backend/src/main/java/com/pofo/backend/domain/mapper/ProjectMapper.java
@@ -8,6 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
     @Mapping(source = "id", target = "projectId")
+    @Mapping(source = "deleted", target = "isDeleted")
     @Mapping(target = "skills", expression = "java(project.getProjectSkills().stream().map(ps -> ps.getSkill().getName()).collect(java.util.stream.Collectors.toList()))")
     @Mapping(target = "tools", expression = "java(project.getProjectTools().stream().map(pt -> pt.getTool().getName()).collect(java.util.stream.Collectors.toList()))")
     ProjectDetailResponse projectToProjectDetailResponse(Project project);
