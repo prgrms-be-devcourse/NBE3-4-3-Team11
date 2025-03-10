68746796427d3950772a61a9c3a97e2097021d86
seeyeon <gulf72pr@swu.ac.kr>
2025-03-10 15:33:45 +0900

    Refactor: BaseTime, BaseEntity 코틀린으로 전환

--- backend/src/main/java/com/pofo/backend/common/jpa/entity/BaseTime.kt
+++ backend/src/main/java/com/pofo/backend/common/jpa/entity/BaseTime.kt
@@ -1,26 +1,18 @@
-package com.pofo.backend.common.jpa.entity;
+package com.pofo.backend.common.jpa.entity
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-@Getter
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener::class)
 @MappedSuperclass
-public class BaseTime extends BaseEntity {
+abstract class BaseTime : BaseEntity() {
 	@CreatedDate
-	@Setter(AccessLevel.PRIVATE)
-	private LocalDateTime createdAt;
+	lateinit var createdAt: LocalDateTime
 
 	@LastModifiedDate
-	@Setter(AccessLevel.PRIVATE)
-	private LocalDateTime updatedAt;
+	lateinit var updatedAt: LocalDateTime
 }
