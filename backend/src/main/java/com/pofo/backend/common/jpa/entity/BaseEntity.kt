cd9de0a044870652e02cf133833f11f38d4567b0
loong6138 <loong6138@gmail.com>
2025-03-10 15:13:02 +0900

    Refactor: BaseTime, BaseEntity 코틀린으로 전환

--- backend/src/main/java/com/pofo/backend/common/jpa/entity/BaseEntity.kt
+++ backend/src/main/java/com/pofo/backend/common/jpa/entity/BaseEntity.kt
@@ -1,19 +1,13 @@
-package com.pofo.backend.common.jpa.entity;
+package com.pofo.backend.common.jpa.entity
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import lombok.*;
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
 
-@Getter
-@EqualsAndHashCode
 @MappedSuperclass
-public class BaseEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-	@Setter(AccessLevel.PROTECTED)
-	@EqualsAndHashCode.Include
-	private Long id;
-
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
 }
